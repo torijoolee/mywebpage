72bd947f1391dd92f1e55f4d3a9cfad47c342002
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-05-30 18:12:13 +0900

    remove on

--- js/gnb.js
+++ js/gnb.js
@@ -3,6 +3,7 @@
   const gnbHeight = gnbElem.getBoundingClientRect().height;
   const gnbMoBtn = document.querySelector(".gnb-button");
   const gnbMo = document.querySelector(".gnb-mo");
+  const moContent = document.querySelector(".mo-gnb-content");
   function handleNavbarBg() {
     if (window.scrollY > gnbHeight) {
       gnbElem.classList.add("On");
@@ -14,6 +15,11 @@
   function handleMoGnb() {
     gnbMo.classList.toggle("On");
   }
+
+  function closeMoGnb() {
+    gnbMo.classList.remove("On");
+  }
+  moContent.addEventListener("click", closeMoGnb);
   gnbMoBtn.addEventListener("click", handleMoGnb);
   window.addEventListener("scroll", handleNavbarBg);
 })();
