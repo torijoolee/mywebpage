8f301b7e122cc02ac3fcec10dd7f5393c8bbd7ff
leekyoungjoo <120783305+torijoolee@users.noreply.github.com>
2023-05-30 20:17:40 +0900

    JS: portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,21 @@
 (() => {
   const workBtnWrap = document.querySelector(".btn-wrap");
   const imgWrap = document.querySelectorAll(".img-item");
+  const portfolioContent = document.querySelector(".portfolio-content");
 
   function handleTarget(e) {
     const filter = e.target.dataset.filter;
     if (filter == null) return;
     for (let i = 0; i < imgWrap.length; i++) {
-      if (filter == "*" || filter == imgWrap[i].dataset.type) {
-        imgWrap[i].classList.remove("On");
-      } else {
-        imgWrap[i].classList.add("On");
-      }
+      portfolioContent.classList.add("On");
+      setTimeout(() => {
+        if (filter == "*" || filter == imgWrap[i].dataset.type) {
+          imgWrap[i].classList.remove("On");
+        } else {
+          imgWrap[i].classList.add("On");
+        }
+        portfolioContent.classList.remove("On");
+      }, 300);
     }
   }
   workBtnWrap.addEventListener("click", handleTarget);
